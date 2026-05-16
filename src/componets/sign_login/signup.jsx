2ccabaecfa0f495083746dd7484dec081205da90
Mohd Muqeem Alam <mohmuqeemalam@gmail.com>
2026-05-17 02:36:52 +0530

    css file include

--- src/componets/sign_login/signup.jsx
+++ src/componets/sign_login/signup.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import { useNavigate } from "react-router";
 import { BuyItem } from "../context/store";
 import FourDigitInpu from "./otp";
-import styles from "./sign_login/signUp.module.css";
+import styles from "./signUp.module.css";
 
 function SignUp() {
   const { setUser, user } = useContext(BuyItem);
